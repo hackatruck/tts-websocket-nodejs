3e16e753a6d0dcd9503fa6600618cdf3e324c232
Leandro David <leandrodvd@gmail.com>
2017-07-06 22:04:26 -0300

    minor refactor

--- app.js
+++ app.js
@@ -3,16 +3,13 @@ var WebSocket = require('ws');
 
 var voice = config.voice ||  "en-US_AllisonVoice";
 console.log("voice:" + voice);
+
 var token = config.token || "";
 console.log("token:" + token);
+
 var wsURI = 'wss://stream.watsonplatform.net/text-to-speech/api/v1/synthesize?voice=' +
   voice + '&watson-token=' + token + '&timmings=[words] ';
 console.log("WebSocket URI:" + wsURI);
-var websocket = new WebSocket(wsURI);
-websocket.onopen = function(evt) { onOpen(evt) };
-websocket.onclose = function(evt) { onClose(evt) };
-websocket.onmessage = function(evt) { onMessage(evt) };
-websocket.onerror = function(evt) { onError(evt) };
 
 function onOpen(evt) {
   console.log("WebSocket Opened - synthesize a text message");
@@ -45,3 +42,9 @@ function onClose(evt) {
 function onError(evt) {
   console.log("Error on websocket");
 }
+
+var websocket = new WebSocket(wsURI);
+websocket.onopen = function(evt) { onOpen(evt) };
+websocket.onclose = function(evt) { onClose(evt) };
+websocket.onmessage = function(evt) { onMessage(evt) };
+websocket.onerror = function(evt) { onError(evt) };
